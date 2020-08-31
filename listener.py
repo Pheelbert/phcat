acfb69696034b6a7b12f9e0611d6ac13ffc1e0de
Alexandre Philbert <alexandre.philbert@hotmail.com>
2020-08-31 13:32:12 -0400

    Working multi-line output but slow (using timeout)

--- listener.py
+++ listener.py
@@ -2,16 +2,26 @@ import pwn
 
 EXPECTED_PROMPT = b'$ '
 LISTENING_PORT = 9001
+COMMAND_TIMEOUT = 1 # Waits at least 1 second when 'until' string doesn't match
 
 def main():
+    commands = [
+        'whoami',
+        'hostname',
+        'uname -a',
+        'sudo -l'
+    ]
+
+    commands_output = {}
     with pwn.listen(LISTENING_PORT).wait_for_connection() as client:
-        whoami = send_command_read_output(client, b'whoami')
-        hostname = send_command_read_output(client, b'hostname')
-        uname = send_command_read_output(client, b'uname -a')
-        print(f'whoami -> {whoami}')
-        print(f'hostname -> {hostname}')
-        print(f'uname -> {uname}')
+        for command in commands:
+            output = send_command_read_output(client, command.encode())
+            commands_output[command] = output
+
         #client.interactive()
+    
+    for command, output in commands_output.items():
+        print(f'{command}\n--------\n{output}\n--------\n')
 
 def ignore_until_prompt(client, prompt=EXPECTED_PROMPT):
     if not isinstance(prompt, bytes):
@@ -27,7 +37,7 @@ def send_command(client, command):
 
     client.sendline(command)
 
-def send_command_read_output(client, command, prompt=EXPECTED_PROMPT):
+def send_command_read_output(client, command, prompt=EXPECTED_PROMPT, timeout=COMMAND_TIMEOUT):
     if not isinstance(command, bytes):
         print('Command must be bytes')
         exit()
@@ -39,9 +49,16 @@ def send_command_read_output(client, command, prompt=EXPECTED_PROMPT):
     ignore_until_prompt(client, prompt)
     client.sendline(command)
     client.recvuntil(b'\n')
-    output_bytes = client.recvuntil(b'\n')
-    output_decoded = output_bytes.decode("utf-8")
-    return output_decoded.strip()
+
+    output = ''
+    while True:
+        output_line = client.recvuntil(b'\n', timeout=timeout).decode('utf-8')
+        if not output_line:
+            break
+
+        output += output_line
+
+    return output.strip()
 
 if __name__ == '__main__':
     main()
